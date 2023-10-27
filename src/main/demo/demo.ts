2a839c1382c003ce5d7123183a1677590dc14175
wanooknox <wanooknox@gmail.com>
2023-10-26 23:56:54 -0600

    implement three examples for the docs

--- src/main/demo/demo.ts
+++ src/main/demo/demo.ts
@@ -1,95 +1,208 @@
 import { Clock, ClockState, Clockify, Duration, DurationParams } from '../clockify.js';
-// import { Clockify } from '../clock.js';
-// import { clockifyDuration } from "../clock-utils.js";
+import { createApp, ref } from 'vue';
+
+createApp({
+    setup() {
+        const c = new Clock();
+        let clockTxt = ref(Clockify.duration(c.state.time));
+        let statusTxt = ref('Running...');
+
+        c.configure({ target: Duration.of(30, 'seconds'), interval: Duration.of(500, 'milliseconds') });
+        c.events.subscribe('updated', (state:ClockState) => {            
+            clockTxt.value = Clockify.duration(state.time); 
+        });
+        c.events.subscribe('finished', (state:ClockState) => {            
+            clockTxt.value = Clockify.duration(state.time);
+            statusTxt.value = "FINISHED!";
+        });
+
+        c.start();
+    
+        return {
+            clockTxt,
+            statusTxt
+        }
+    }
+}).mount('#ex1');
+
+
+createApp({
+    setup() {
+        const c = new Clock();
+        let clockTxt = ref(Clockify.duration(c.state.time));
+        let phaseTxt = ref(c.state.phase.toLocaleUpperCase());
+
+        c.configure({ target: Duration.of(365, 'days'), interval: Duration.of(100, 'milliseconds') });
+        c.events.subscribe('started', (state:ClockState) => {
+            phaseTxt.value = state.phase.toLocaleUpperCase();
+        });
+        c.events.subscribe('paused', (state:ClockState) => {
+            phaseTxt.value = state.phase.toLocaleUpperCase();
+        });
+        c.events.subscribe('stopped', (state:ClockState) => {
+            phaseTxt.value = state.phase.toLocaleUpperCase();
+        });
+        c.events.subscribe('updated', (state:ClockState) => {
+            clockTxt.value = Clockify.duration(state.time, ['minutes', 'seconds', 'milliseconds']);
+        });
+
+        function start() {
+            c.start();
+        }
+        function pause() {
+            c.pause();
+        }
+        function stop() {
+            c.stop();
+        }
+    
+        return {
+            phaseTxt, 
+            clockTxt,
+            start, 
+            stop, 
+            pause
+        }
+    }
+}).mount('#ex2');
+
+createApp({
+    setup() {
+        const c = new Clock();
+        let clockTxt = ref('Not Started');
+        let phaseTxt = ref(c.state.phase.toLocaleUpperCase());
+
+        c.configure({ mode: 'countdown', initial: Duration.of(5, 'minutes'), interval: Duration.of(100, 'milliseconds') });
+        
+        c.events.subscribe('started', (state:ClockState) => {
+            phaseTxt.value = state.phase.toLocaleUpperCase();
+            clockTxt.value = Clockify.duration(state.time);
+        });
+        c.events.subscribe('paused', (state:ClockState) => {
+            phaseTxt.value = state.phase.toLocaleUpperCase();
+        });
+        c.events.subscribe('stopped', (state:ClockState) => {
+            phaseTxt.value = state.phase.toLocaleUpperCase();
+        });
+        c.events.subscribe('finished', (state:ClockState) => {
+            phaseTxt.value = state.phase.toLocaleUpperCase();            
+            clockTxt.value = Clockify.duration(state.time);
+        });
+        c.events.subscribe('updated', (state:ClockState) => {
+            clockTxt.value = Clockify.duration(state.time);
+        });
+
+        function start() {
+            c.start();
+        }
+        function pause() {
+            c.pause();
+        }
+        function stop() {
+            c.stop();
+        }
+
+        c.start();
+    
+        return {
+            phaseTxt, 
+            clockTxt,
+            start, 
+            stop, 
+            pause
+        }
+    }
+}).mount('#ex3')
 
 
 
-const testdur1 = Duration.of(123456789876, 'milliseconds');
-console.log(testdur1.asValues());
-console.log(Clockify.duration(testdur1));
-console.log(Clockify.duration(testdur1, ['hours', 'minutes', 'seconds', 'milliseconds']));
-console.log(Clockify.milliseconds(987654321, ['days', 'hours', 'minutes', 'seconds', 'milliseconds']));
-const testdur2 = Duration.of(10067, 'milliseconds');
-console.log(testdur2.asValues());
-console.log(Clockify.duration(testdur2));
-console.log(Clockify.duration(testdur2, ['minutes', 'seconds', 'milliseconds'], ';'));
-const params:DurationParams = {}
-const dur3 = new Duration(params)
+
+// const testdur1 = Duration.of(123456789876, 'milliseconds');
+// console.log(testdur1.asValues());
+// console.log(Clockify.duration(testdur1));
+// console.log(Clockify.duration(testdur1, ['hours', 'minutes', 'seconds', 'milliseconds']));
+// console.log(Clockify.milliseconds(987654321, ['days', 'hours', 'minutes', 'seconds', 'milliseconds']));
+// const testdur2 = Duration.of(10067, 'milliseconds');
+// console.log(testdur2.asValues());
+// console.log(Clockify.duration(testdur2));
+// console.log(Clockify.duration(testdur2, ['minutes', 'seconds', 'milliseconds'], ';'));
+// const params:DurationParams = {}
+// const dur3 = new Duration(params)
 
 
 
-let c = new Clock();
-console.log(c.state); 
-c.start();
-console.log(c.state); 
-c.pause();
-console.log(c.state); 
-c.stop(); 
-console.log(c.state);
+// let c = new Clock();
+// console.log(c.state); 
+// c.start();
+// console.log(c.state); 
+// c.pause();
+// console.log(c.state); 
+// c.stop(); 
+// console.log(c.state);
 
-let d = Duration.of(65, 'seconds');
-console.log(d.in('minutes'));
-console.log(d.as('minutes'));
+// let d = Duration.of(65, 'seconds');
+// console.log(d.in('minutes'));
+// console.log(d.as('minutes'));
 
-let d2 = Duration.parse("5 minutes").in('seconds');
+// let d2 = Duration.parse("5 minutes").in('seconds');
 
-// c = new Clock({ mode: 'countdown', initial: Duration.of(1, 'minutes') });
-c.configure({ 
-    mode: 'countdown',
-    initial: Duration.of(5, 'seconds'),
-    target: Duration.of(1, 'seconds'),
-    interval: Duration.of(500, 'milliseconds')
-});
-c.events.subscribe('started', (state:ClockState) => {
-    console.log(`STARTED }}}}}}}}}}`)
-    console.log(state)
-    console.log(state.time.in('seconds'))
-    console.log(state.time.as('milliseconds'))
-    console.log(Clockify.duration(state.time))
-    console.log(`{{{{{{{{{ Started`)
-});
-c.events.subscribe('updated', (state:ClockState) => {
-    console.log(`UPDATED >>>>>>>>>>`)
-    console.log(state)
-    console.log(state.time.in('seconds'))
-    console.log(state.time.as('milliseconds'))
-    console.log(Clockify.duration(state.time, ['minutes', 'seconds', 'milliseconds']))
-    console.log(`<<<<<<<<<<<<< UPDATED`)
-});
+// // c = new Clock({ mode: 'countdown', initial: Duration.of(1, 'minutes') });
+// c.configure({ 
+//     mode: 'countdown',
+//     initial: Duration.of(5, 'seconds'),
+//     target: Duration.of(1, 'seconds'),
+//     interval: Duration.of(500, 'milliseconds')
+// });
+// c.events.subscribe('started', (state:ClockState) => {
+//     console.log(`STARTED }}}}}}}}}}`)
+//     console.log(state)
+//     console.log(state.time.in('seconds'))
+//     console.log(state.time.as('milliseconds'))
+//     console.log(Clockify.duration(state.time))
+//     console.log(`{{{{{{{{{ Started`)
+// });
+// c.events.subscribe('updated', (state:ClockState) => {
+//     console.log(`UPDATED >>>>>>>>>>`)
+//     console.log(state)
+//     console.log(state.time.in('seconds'))
+//     console.log(state.time.as('milliseconds'))
+//     console.log(Clockify.duration(state.time, ['minutes', 'seconds', 'milliseconds']))
+//     console.log(`<<<<<<<<<<<<< UPDATED`)
+// });
 
-c.events.subscribe('stopped', (state:ClockState) => {
-    console.log('STOPPED ::::::::::');
-    console.log(state)
-    console.log(state.time.in('seconds'))
-    console.log(state.time.as('milliseconds'))
-});
-c.events.subscribe('finished', (state:ClockState) => {
-    console.log('FINISHED');
-    console.log(state)
-    console.log(state.time.in('seconds'))
-    console.log(state.time.as('milliseconds'))
+// c.events.subscribe('stopped', (state:ClockState) => {
+//     console.log('STOPPED ::::::::::');
+//     console.log(state)
+//     console.log(state.time.in('seconds'))
+//     console.log(state.time.as('milliseconds'))
+// });
+// c.events.subscribe('finished', (state:ClockState) => {
+//     console.log('FINISHED');
+//     console.log(state)
+//     console.log(state.time.in('seconds'))
+//     console.log(state.time.as('milliseconds'))
 
-    c.events.unsubscribeAll();
-    c.configure({initial: Duration.of(0, 'seconds'), 
-    target: Duration.of(1000, 'seconds')})
+//     c.revert();
+//     c.configure({initial: Duration.of(0, 'seconds'), 
+//     target: Duration.of(1000, 'seconds')})
 
-    c.events.subscribe('updated', (state:ClockState) => {
-        console.log(`UPDATED >>>>>>>>>> NEW`)
-        console.log(state)
-        console.log(state.time.in('seconds'))
-        console.log(state.time.as('milliseconds'))
-        console.log(`<<<<<<<<<<<<< UPDATED`)
-    });
+//     c.events.subscribe('updated', (state:ClockState) => {
+//         console.log(`UPDATED >>>>>>>>>> NEW`)
+//         console.log(state)
+//         console.log(state.time.in('seconds'))
+//         console.log(state.time.as('milliseconds'))
+//         console.log(`<<<<<<<<<<<<< UPDATED`)
+//     });
 
-    console.log('restart:')
-    // c.start();
-    console.log(c.state)
-});
-// c.configure({ mode: 'stopwatch' });
-c.start();
-c.pause();
-c.pause();
-c.start();
+//     console.log('restart:')
+//     // c.start();
+//     console.log(c.state)
+// });
+// // c.configure({ mode: 'stopwatch' });
+// c.start();
+// c.pause();
+// c.pause();
+// c.start();
 // c.pause();
 
 
